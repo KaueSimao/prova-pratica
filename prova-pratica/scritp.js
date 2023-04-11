bcc9cf7cee3eb1a5ad37c58c01304dc78910d839
kauesimao <kauesimao1234@gmail.com>
2023-04-11 15:37:14 -0300

    prova-pratica

--- prova-pratica/scritp.js
+++ prova-pratica/scritp.js
@@ -1,15 +1,17 @@
-function converter(ct){        
-    const valor1 = parseFloat(document.getElementById("euro").value);
-    const valor2 = parseFloat(document.getElementById("dolar").value);
-    const valor3 = parseFloat(document.getElementById("libras").value);
-    const valor4 = parseFloat(document.getElementById("valor").value);
+function conversao(cs){        
+    var valor1 = parseFloat(document.getElementById("euro").value);
+    var valor2 = parseFloat(document.getElementById("dolar").value);
+    var valor3 = parseFloat(document.getElementById("libras").value);
+    var valor4 = parseFloat(document.getElementById("valor").value);
+    var resposta = parseFloat(document.getElementById("resposta").value);
     var resposta_final;
 
-    switch (ct) {
+    switch (cs) {
 
         case "*":
-            resposta_final = euro * valor
-
+            resposta_final = valor1 * valor4
+            resposta_final = valor2 * valor4
+            resposta_final = valor3 * valor4
             break
 
     }
