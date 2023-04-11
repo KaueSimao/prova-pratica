a4816693399f9faa26df6dbf2954f1422577d502
kauesimao <kauesimao1234@gmail.com>
2023-04-11 16:17:44 -0300

    prova-pratica

---
+++ prova-pratica/script.js
@@ -0,0 +1,6 @@
+function converter(){
+    var dolar = document.getElementById("dolar");
+    var valor = document.getElementById("real");
+    var libra = document.getElementById("libras");
+    var euro = document.getElementById("euro");
+}
